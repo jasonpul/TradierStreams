86b7b702c7d2d98a79a7fa837137d95b7639f994
Akbar Amin <akbar.amin917@gmail.com>
2020-12-18 20:11:12 -0800

    Update historical.py

--- historical.py
+++ historical.py
@@ -1,87 +1,38 @@
-from urllib.parse import urlencode
-from typing import Callable, Any, List
-
-import utils, json, time, logging, requests, urllib3, streamz
+import logging, utils 
+from streamz import Stream
+from helpers import History, Client
 
 utils.consolelogger(10)
-urllib3.disable_warnings()
 
-
-paths = {
-    "brokerage": "https://api.tradier.com/v1/markets/",
-    "sandbox": "https://sandbox.tradier.com/v1/markets/",
-    "daily": "history",
-    "weekly": "history",
-    "monthly": "history",
-    "1min": "timesales",
-    "5min": "timesales",
-    "15min": "timesales",
-    "tick": "timesales"
-}
-
-
-class TM:
-
-    def __init__(self, API: str, endpoint: str, delay: float = 0.25, callback: Callable[[dict], Any] = print):
+class Market:
+    def __init__(self, key: str, endpoint: str):
         """ 
         Programmatic control over fetching historical data from Tradier's Market Data API.
 
-        Register multiple tickers with the same parameters and block until the 'request' method is called.
+        Can be used to register multiple tickers with the same parameters.
 
         Args:
-            API (str): (required) a valid Sandbox or Brokerage API key
+            key (str): (required) a valid Sandbox or Brokerage API key
             endpoint (str): (required) corresponding API endpoint, either of "brokerage" or "sandbox"
-            delay (float): time delay between requests 
-            callback (callable): callback to run for every response received 
-
 
         Example usage:
 
         .. code-block:: python
 
-            results = []
-
-            market = TM(API_key = "...", endpoint = "brokerage", callback = results.append)
-            market.register("TSLA", "MSFT", "AAPL", interval = "D")
-            market.register("SPY", "VIX", interval = "1min", start = "2020-12-09", end = "2020-12-13")
+            market = Market(key = "API_KEY" endpoint = "API_ENDPOINT")
+            market.register(["TSLA", "MSFT", "AAPL"], interval = "daily")
+            market.register(["SPY", "VIX"], interval = "1min", start = "2020-12-09", end = "2020-12-13")
             market.request()
+
+            print(market.results)
         """
 
-        self.session = self._create_session(API, endpoint) 
-        self.endpoint = endpoint
-        self.callback = callback
-        self.delay = delay
-
+        self.httpclient = Client(key, endpoint)
+        
         self.tasks = []
+        self.results = []
 
-    @staticmethod
-    def _create_session(API: str, endpoint: str):
-        session = requests.Session()
-        session.headers.update({"Authorization":"Bearer %s" % API, "Accept": "application/json"})
-        session.verify = False
-
-        retry = urllib3.util.Retry(total = 3, status = 3, status_forcelist = [403], backoff_factor = 4)
-        session.mount(endpoint, requests.adapters.HTTPAdapter(pool_connections = 1, max_retries = retry, pool_maxsize = 500)) 
-        return session
-
-    def _create_URL(self, symbol, **params):
-        params["symbol"] = symbol
-        interval = params["interval"]
-
-        return paths[self.endpoint] + paths[interval] + "?" + urlencode(params)
-
-    def request(self):
-        source = streamz.Stream()
-        source.rate_limit(self.delay).map(self.session.get).map(lambda x: x.json()).sink(self.callback)
-
-        for URL in self.tasks:
-            logging.debug(f"Task: {URL}")
-            source.emit(URL)
-            
-        self.tasks.clear()
-
-
-    def register(self, symbols = List[str], interval = str, **params):
+    def register(self, symbols = list, interval = str, **kwargs):
         """ 
         Maps parameters to multiple symbols 
 
@@ -89,14 +40,23 @@ class TM:
             symbols (list): (required) equity or option ticker symbols
             interval (str): (required) one of "daily", "weekly", "monthly", "1min", "5min", "15min", "tick"
 
-        Params:
+        Keyword Args:
             start, end = time strings in the format YYYY-MM-DD HH:MM 
         """
 
-        source = streamz.Stream()
-        source.map(self._create_URL, **params).sink(self.tasks.append)
-
         for symbol in symbols:
-            logging.debug(f"Registering: {symbol} ({interval})")
-            source.emit(symbol)
-            
+            self.tasks.append(repr(History(symbol, interval, **kwargs)))
+        
+        logging.debug("[-->] registered %d URLs (current queue size: %d)" % (len(symbols), len(self.tasks)))
+
+    def request(self):
+        """ Starts making requests and stores results in a list """
+
+        fetch = Stream()
+        fetch.rate_limit(0.25).map(self.httpclient.s.get).map(lambda x: x.json()).sink(self.results.append)
+        
+        for task in self.tasks:
+            fetch.emit(task)    
+            logging.debug("[<--] %s" % task.split("?")[1])
+        
+        self.tasks.clear()
